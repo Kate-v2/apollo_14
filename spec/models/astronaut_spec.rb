d01b0c798372d8ef9503b166a40052cb92320d84
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-12 10:25:58 -0600

    Given tests are functional, relationships established

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,43 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'Calculations' do
+
+    before(:each) do
+
+      @astronaut1 = Astronaut.create!(name: "Astronaut 1", age: 10, job: "Title 1")
+      @astronaut2 = Astronaut.create!(name: "Astronaut 2", age: 20, job: "Title 2")
+
+      @mission1 = Mission.create!(title: "Mission 1", time_in_space: 100)
+      @mission2 = Mission.create!(title: "Mission 2", time_in_space: 200)
+      @mission3 = Mission.create!(title: "Mission 3", time_in_space: 300)
+
+      AstronautMission.create!(astronaut: @astronaut1, mission: @mission1)
+      AstronautMission.create!(astronaut: @astronaut1, mission: @mission2)
+      AstronautMission.create!(astronaut: @astronaut1, mission: @mission3)
+
+      AstronautMission.create!(astronaut: @astronaut2, mission: @mission2)
+      AstronautMission.create!(astronaut: @astronaut2, mission: @mission3)
+
+    end
+
+
+
+
+    # Would rather this be dynamic to astronauts
+    # shown, but that's not a task
+    it 'can average age of all astronauts' do
+
+
+
+    end
+
+
+  end
+
+
+
+
+
 end
